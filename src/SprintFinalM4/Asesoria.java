531ad133685a007f3d9cd83878d95e380763ec2a
Meneazus <nicolas.gajardo558@gmail.com>
2024-08-27 11:40:01 -0400

    Initial commit

---
+++ src/SprintFinalM4/Asesoria.java
@@ -0,0 +1,16 @@
+package SprintFinalM4;
+
+/**
+ * Clase Asesoría que funciona en parte como interfase
+ * 
+ * @author Nicolás Gajardo
+ * @author Jorge Lira
+ * @author Beatriz Maldonado
+ * @author Diego Rivera
+ *
+ * @version 1.0
+ */
+
+public interface Asesoria {
+	void analizarUsuario();
+}
